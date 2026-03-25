3ac0ac6aae35596946bd16439f8ebb04a2944fae
ReedBelca <reedbelca55@gmail.com>
2026-03-25 16:14:01 +0000

    feat: configuration initiale du backend avec Prisma (schéma v7) et les modules de base (utilisateurs, biens, réservations, paiements)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { BiensModule } from './biens/biens.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { PaiementsModule } from './paiements/paiements.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, BiensModule, ReservationsModule, PaiementsModule],
   controllers: [AppController],
   providers: [AppService],
 })
